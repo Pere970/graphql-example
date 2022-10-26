45180926b93bb4860ddd6ae61858c060eaa35b48
Pere Palou <palouponsperejoan20@gmail.com>
2022-10-26 12:05:24 +0200

    Added some dummy data to be queried

--- server.js
+++ server.js
@@ -2,11 +2,28 @@ const express = require('express')
 
 const { graphqlHTTP } = require('express-graphql');
 const { 
-    buildSchema, 
     GraphQLSchema,
     GraphQLObjectType,
     GraphQLString
 } = require('graphql');
+
+const publishers = [
+    { id: 1, name: 'Activision'},
+    { id: 2, name: 'PlayStation Studios'},
+    { id: 3, name: 'Nintendo'},
+    { id: 4, name: 'Microsoft'}
+]
+
+const games = [
+    { id: 1, name: 'Call of Duty', publisherId: 1},
+    { id: 2, name: 'Destiny', publisherId: 1},
+    { id: 3, name: 'The Last of Us', publisherId: 2},
+    { id: 4, name: 'God of War', publisherId: 2},
+    { id: 5, name: 'Pokemon', publisherId: 3},
+    { id: 6, name: 'Super Mario Bros', publisherId: 3},
+    { id: 7, name: 'Halo', publisherId: 4},
+    { id: 8, name: 'Microsoft Flight Simulator', publisherId: 4}
+]
 
 const schema = new GraphQLSchema ({
     query: new GraphQLObjectType({
