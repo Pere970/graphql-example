ec0a7fce276d15d4d2aa7d71a09edcaafacf0b4f
Pere Palou <palouponsperejoan20@gmail.com>
2022-10-26 11:58:09 +0200

    Added a default HelloWorld schema to GraphQL to be queried

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
 const express = require('express')
+
+const { graphqlHTTP } = require('express-graphql');
+const { 
+    buildSchema, 
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString
+} = require('graphql');
+
+const schema = new GraphQLSchema ({
+    query: new GraphQLObjectType({
+        name: 'HelloWorld',
+        fields: () => ({
+            message: {
+                type: GraphQLString,
+                resolve: () => 'Hello World'
+            }
+        })
+    })
+})
+
 const app = express()
 
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}))
 app.listen(5001., () => console.log('Server running'))
